9675da48ea594d84d0827271d3edf234576e9ed1
Alexa <alexafdeveloper@gmail.com>
2019-08-14 22:53:31 +0000

    environ change to getenv

--- models/__init__.py
+++ models/__init__.py
@@ -9,12 +9,11 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from os import environ
+from os import getenv
 
-try:
-    if environ['HBNB_TYPE_STORAGE'] == 'db':
-        storage = DBStorage()
-        storage.reload()
-except:
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()
+
+else:
     storage = FileStorage()
-    storage.reload()
+storage.reload()
