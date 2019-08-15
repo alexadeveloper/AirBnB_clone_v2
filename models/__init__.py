a5249e6379e1b67ab973d535eb745a87356fc35a
carlosariasf <733@holbertonschool.com>
2019-08-15 16:17:17 +0000

    Fix __init__

--- models/__init__.py
+++ models/__init__.py
@@ -9,10 +9,10 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from os import environ
+from os import getenv
 
 
-if environ['HBNB_TYPE_STORAGE'] == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     storage = DBStorage()
     storage.reload()
 else:
