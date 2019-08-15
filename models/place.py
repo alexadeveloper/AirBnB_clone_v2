16bc391814a52f8195776cf7f2efd515fb0796ab
Alexa Fonseca <alexafdeveloper@gmail.com>
2019-08-15 23:49:41 +0000

    table place_amenities

--- models/place.py
+++ models/place.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """This is the place class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id',
+                             String(60),
+                             ForeignKey=("places.id")
+                             primary_key=True,
+                             nullable=False),
+                      Column('amenity_id',
+                             String(60),
+                             ForeignKey=("amenities.id")
+                             primary_key=True,
+                             nullable=False))
+                      
 
 class Place(BaseModel, Base):
     """This is the class for Place
