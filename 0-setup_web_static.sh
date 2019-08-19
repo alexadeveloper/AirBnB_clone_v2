c7ddba95cf3875a1b87bc2ba11457ac6986b8f72
Alexa <alexafdeveloper@gmail.com>
2019-08-19 18:57:47 +0000

    change hello world to Holberton School

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHello world\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 content="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
